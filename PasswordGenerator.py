a749fa25d5fe2547ec7ca94c83d6ac45a7774c21
Timothy Fee <101140703+timothfee@users.noreply.github.com>
2023-06-05 12:11:13 -0500

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,15 +1,22 @@
+#import the random module.
 import random
+
+#Create the list of possible letters/numbers/symbols.
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+#User inputs that are stored as variables.
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#Creating the initial list of variables specified by user.
 password_list = []
 
+#For loops that choose a random value from the lists above and stores it in password_list.
+#We append the value to the list.
 for char in range(1, nr_letters + 1):
     password_list.append(random.choice(letters))
 
@@ -19,11 +26,15 @@ for char in range(1, nr_symbols + 1):
 for char in range(1, nr_numbers + 1):
     password_list.append(random.choice(numbers))
 
+#We use the random module to shuffle the list making it random.
 random.shuffle(password_list)
 
+#Creating the variable that will be the generated password.
 password = ''
 
+#For each character in password_list, add it to the password string.
 for char in password_list:
     password += char
 
+#Print the results.
 print(f"Your password is: {password}")
